9a94b2fb197a353635f0c4eec993945d58347447
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-01-14 20:54:16 +0200

    Add files via upload

---
+++ DragBall.java
@@ -0,0 +1,57 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/** Create an applet that draws an oval which 
+the user can drag around the applet */
+
+public class DragBall extends Applet{
+	
+	int x;
+	int y;
+	int r=25;
+
+	
+
+	public void init(){
+	    x=250;
+	    y=250;
+			
+        this.requestFocus();
+		this.addMouseListener(new MouseAdapter() {
+		public void mouseClicked(MouseEvent e) { 
+		    x=e.getX();
+			y=e.getY();
+			repaint();
+		}
+		public void mousePressed(MouseEvent e) { 
+		    
+		    x=e.getX();
+			y=e.getY();
+			repaint();  
+		}
+	    public void mouseReleased(MouseEvent e) { 
+		    
+		    x=e.getX();
+			y=e.getY();
+			repaint();  
+		}
+		});
+		this.addMouseMotionListener(new MouseAdapter(){
+	    public void mouseDragged(MouseEvent e) { 
+		    
+		    x=e.getX();
+			y=e.getY();
+			repaint(); 
+		} 
+	    });
+	}	
+		
+	
+	public void paint (Graphics g){
+		 g.setColor(Color.red);
+		 g.drawOval(x,y,r*2,r*2);
+		 g.fillOval(x,y,r*2,r*2);
+	
+	}	
+}
